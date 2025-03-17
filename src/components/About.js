e6a496e6f2862f7dd3bcdc988c627a7a09d90e36
rcassettari <rcassettari@gmail.com>
2025-03-17 10:44:25 -0300

    Turning app more generic, to be used as test example

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import DadosHenrylle from "./DadosHenrylle";
 const About = () => {
   return (
     <div>
-      <h4>Versão 3.2.0</h4>
-      <h5>BIA 27/01 a 02/02/2025</h5>
+      <h4>Tarefas - Versão 1.0</h4>
       <Link to="/">Voltar</Link>
-      <DadosHenrylle />
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA 2025",
+  title: "Tarefas",
 };
 
 export default Header;
